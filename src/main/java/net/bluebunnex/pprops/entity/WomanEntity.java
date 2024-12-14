1be4b3ae029da891cac8a825b8e2d56391eee625
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2024-12-14 06:38:22 -0500

    update woman behaviour

--- src/main/java/net/bluebunnex/pprops/entity/WomanEntity.java
+++ src/main/java/net/bluebunnex/pprops/entity/WomanEntity.java
@@ -1,7 +1,9 @@
 package net.bluebunnex.pprops.entity;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class WomanEntity extends AnimalEntity {
@@ -10,6 +12,33 @@ public class WomanEntity extends AnimalEntity {
         super(world);
 
         this.texture = "/assets/pprops/stationapi/textures/entity/woman1.png";
+
+        this.maxHealth = 20;
+        this.health = 20;
+    }
+
+    @Override
+    public boolean interact(PlayerEntity player) {
+        super.onPlayerInteraction(player);
+
+        ItemStack held = player.inventory.getSelectedItem();
+
+        if (this.health != this.maxHealth && held != null && held.getItem() == Block.ROSE.asItem()) {
+
+            this.heal(4);
+
+            for (int i=0; i<5; i++) {
+                this.world.addParticle("smoke", this.x, this.y + 0.5, this.z, Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+            }
+
+            player.inventory.removeStack(player.inventory.selectedSlot, 1);
+
+            return true;
+
+        } else {
+
+            return false;
+        }
     }
 
     @Override
