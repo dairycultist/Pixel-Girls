4276396ae9ac0eace815c552b6e549e29b82b98f
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2024-12-24 16:51:54 -0500

    update woman behaviour slightly

--- src/main/java/net/bluebunnex/pixelgirls/entity/WomanEntity.java
+++ src/main/java/net/bluebunnex/pixelgirls/entity/WomanEntity.java
@@ -3,6 +3,7 @@ package net.bluebunnex.pixelgirls.entity;
 import net.minecraft.block.Block;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
@@ -13,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class WomanEntity extends AnimalEntity {
 
     private static final int TOTAL_TEXTURE_COUNT = 3;
+    // TODO randomly generate a name for each woman, which renders above their head
 
     private int textureVariant;
 
@@ -30,29 +32,43 @@ public class WomanEntity extends AnimalEntity {
     public boolean interact(PlayerEntity player) {
         super.onPlayerInteraction(player);
 
-        ItemStack held = player.inventory.getSelectedItem();
+        ItemStack heldStack = player.inventory.getSelectedItem();
+        Item heldItem = heldStack != null ? heldStack.getItem() : null;
 
-        // TODO dialogue state machine
-        // for example, she can take a sword from you to use for fighting, say that she's hurt,
-        // thank you for the rose but say she doesn't need healing right now, etc
+        String response;
 
-        if (this.health != this.maxHealth && held != null && held.getItem() == Block.ROSE.asItem()) {
+        // dialogue state machine
+        // TODO take a sword from you to use for fighting
+        if (heldItem == Block.ROSE.asItem()) {
 
-            this.heal(4);
+            if (this.health != this.maxHealth) {
 
-            world.playSound(this, "random.fuse", 1.0F, 1.0F);
-            for (int i=0; i<5; i++) {
-                this.world.addParticle("smoke", this.x, this.y + 0.5, this.z, Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+                this.heal(4);
+
+                world.playSound(this, "random.fuse", 1.0F, 1.0F);
+                for (int i = 0; i < 5; i++) {
+                    this.world.addParticle("smoke", this.x, this.y + 0.5, this.z, Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+                }
+
+                player.inventory.removeStack(player.inventory.selectedSlot, 1);
+
+                response = "Thank you for the rose, I'm feeling better already!";
+
+            } else {
+
+                response = "I appreciate the sentiment sweetie, but I'm in tip top shape already.";
             }
 
-            player.inventory.removeStack(player.inventory.selectedSlot, 1);
+        } else if (this.health < 10) {
 
-            player.sendMessage("[Woman " + this.health + "/20] Thank you for the heals!");
+            response = "I'm not feeling too hot sweetie, do you have a rose?";
 
         } else {
 
-            player.sendMessage("[Woman " + this.health + "/20] Hi sweetie!");
+            response = "Hi sweetie.";
         }
+
+        player.sendMessage("[Woman " + this.health + "/20] " + response);
 
         player.swingHand();
         return true;
@@ -64,7 +80,9 @@ public class WomanEntity extends AnimalEntity {
 
         PlayerEntity player = this.world.getClosestPlayer(this, 16.0);
 
-        // chase nearby player until within XYZ blocks
+        // chase nearby player until within 5 blocks
+        // TODO chase behaviour is overridden when a bed is nearby, following that instead. this is reflected
+        // in their dialogue (this lets you make towns)
         if (player != null && this.canSee(player) && this.getDistance(player) > 5) {
             this.setTarget(player);
         } else {
