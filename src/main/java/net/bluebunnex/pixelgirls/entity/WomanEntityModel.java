4aa7e9a896dd3361e5b98051fc4bf8f17fb50c75
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-01-10 16:57:59 -0500

    work on skin, remove custom animation for now

--- src/main/java/net/bluebunnex/pixelgirls/entity/WomanEntityModel.java
+++ src/main/java/net/bluebunnex/pixelgirls/entity/WomanEntityModel.java
@@ -14,6 +14,7 @@ public class WomanEntityModel extends BipedEntityModel {
         this.breasts = new ModelPart(40, 0);
         this.breasts.addCuboid(0f, 0f, -4f, 8, 4, 4); // position relative to pivot
         this.breasts.setPivot(-4f, 1f, -2f); // pivot (global space)
+        this.breasts.pitch = (float) (Math.PI / 8);
     }
 
     // override cuz we don't want to render the hat
@@ -31,90 +32,5 @@ public class WomanEntityModel extends BipedEntityModel {
         this.breasts.render(scale);
     }
 
-    @Override
-    public void setAngles(float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch, float scale) {
-
-        this.head.yaw = headYaw / 57.295776F;
-        this.head.pitch = headPitch / 57.295776F;
-        this.hat.yaw = this.head.yaw;
-        this.hat.pitch = this.head.pitch;
-        this.rightArm.pitch = MathHelper.cos(limbAngle * 0.6662F + 3.1415927F) * 2.0F * limbDistance * 0.5F;
-        this.leftArm.pitch = MathHelper.cos(limbAngle * 0.6662F) * 2.0F * limbDistance * 0.5F;
-        this.rightArm.roll = 0.0F;
-        this.leftArm.roll = 0.0F;
-        this.rightLeg.pitch = MathHelper.cos(limbAngle * 0.6662F) * 1.4F * limbDistance;
-        this.leftLeg.pitch = MathHelper.cos(limbAngle * 0.6662F + 3.1415927F) * 1.4F * limbDistance;
-        this.rightLeg.yaw = 0.0F;
-        this.leftLeg.yaw = 0.0F;
-        this.breasts.pitch = (float) (Math.PI / 8);
-
-        ModelPart var10000;
-        if (this.riding) {
-            var10000 = this.rightArm;
-            var10000.pitch += -0.62831855F;
-            var10000 = this.leftArm;
-            var10000.pitch += -0.62831855F;
-            this.rightLeg.pitch = -1.2566371F;
-            this.leftLeg.pitch = -1.2566371F;
-            this.rightLeg.yaw = 0.31415927F;
-            this.leftLeg.yaw = -0.31415927F;
-        }
-
-        if (this.leftArmPose) {
-            this.leftArm.pitch = this.leftArm.pitch * 0.5F - 0.31415927F;
-        }
-
-        if (this.rightArmPose) {
-            this.rightArm.pitch = this.rightArm.pitch * 0.5F - 0.31415927F;
-        }
-
-        this.rightArm.yaw = 0.0F;
-        this.leftArm.yaw = 0.0F;
-        if (this.handSwingProgress > -9990.0F) {
-            float var7 = this.handSwingProgress;
-            this.body.yaw = MathHelper.sin(MathHelper.sqrt(var7) * 3.1415927F * 2.0F) * 0.2F;
-            this.rightArm.pivotZ = MathHelper.sin(this.body.yaw) * 5.0F;
-            this.rightArm.pivotX = -MathHelper.cos(this.body.yaw) * 5.0F;
-            this.leftArm.pivotZ = -MathHelper.sin(this.body.yaw) * 5.0F;
-            this.leftArm.pivotX = MathHelper.cos(this.body.yaw) * 5.0F;
-            var10000 = this.rightArm;
-            var10000.yaw += this.body.yaw;
-            var10000 = this.leftArm;
-            var10000.yaw += this.body.yaw;
-            var10000 = this.leftArm;
-            var10000.pitch += this.body.yaw;
-            var7 = 1.0F - this.handSwingProgress;
-            var7 *= var7;
-            var7 *= var7;
-            var7 = 1.0F - var7;
-            float var8 = MathHelper.sin(var7 * 3.1415927F);
-            float var9 = MathHelper.sin(this.handSwingProgress * 3.1415927F) * -(this.head.pitch - 0.7F) * 0.75F;
-            var10000 = this.rightArm;
-            var10000.pitch = (float)((double)var10000.pitch - ((double)var8 * 1.2 + (double)var9));
-            var10000 = this.rightArm;
-            var10000.yaw += this.body.yaw * 2.0F;
-            this.rightArm.roll = MathHelper.sin(this.handSwingProgress * 3.1415927F) * -0.4F;
-        }
-
-        // cute anime girl swaying idle animation component
-        final float moveFac = limbDistance * 8 + 1;
-        final float sin = MathHelper.sin(animationProgress * 0.2f);
-
-        this.rightArm.pitch -= (sin * 0.2F) / moveFac;
-        this.leftArm.pitch  -= (sin * 0.2F) / moveFac;
-        this.rightArm.pivotZ = -(sin * 0.44F) / moveFac;
-        this.leftArm.pivotZ  = -(sin * 0.44F) / moveFac;
-
-        this.rightLeg.pitch -= (sin * 0.1F) / moveFac;
-        this.leftLeg.pitch  -= (sin * 0.1F) / moveFac;
-        this.rightLeg.pivotZ = (sin * 1.3F) / moveFac;
-        this.leftLeg.pivotZ  = (sin * 1.3F) / moveFac;
-
-        this.body.pitch = (sin * 0.13F) / moveFac;
-
-        this.breasts.pitch += (sin * 0.1F) / moveFac;
-        this.breasts.pivotZ = -2f + (sin * 0.44F) / moveFac;
-
-        this.head.pivotZ = -(sin * 0.44F) / moveFac;
-    }
+    // TODO override setAngles to do custom animation
 }
