37cc9440d8d6e0097f542868c3e49fbc25c096a6
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-13 20:51:31 -0400

    Update house generation

--- src/main/java/net/bluebunnex/pixelgirls/mixin/OverworldChunkGeneratorMixin.java
+++ src/main/java/net/bluebunnex/pixelgirls/mixin/OverworldChunkGeneratorMixin.java
@@ -3,6 +3,7 @@ package net.bluebunnex.pixelgirls.mixin;
 import net.bluebunnex.pixelgirls.entity.WomanEntity;
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.ChunkSource;
 import net.minecraft.world.gen.chunk.OverworldChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,74 +27,81 @@ public class OverworldChunkGeneratorMixin {
     @Inject(method = "decorate", at = @At("HEAD"))
     public void decorateMixin(ChunkSource source, int x, int z, CallbackInfo ci) {
 
-//        Biome biome = this.world.method_1781().getBiome(blockX, blockZ);
-//
-//        if (biome == Biome.DESERT) {
-//            world.setBlock(blockX, world.getTopSolidBlockY(blockX, blockZ), blockZ, Block.GOLD_BLOCK.id);
-//        } else {
-//            world.setBlock(blockX, world.getTopSolidBlockY(blockX, blockZ), blockZ, Block.IRON_BLOCK.id);
-//        }
-
         if (random.nextInt(32) == 0) {
 
             int blockX = x * 16 + this.random.nextInt(16);
             int blockZ = z * 16 + this.random.nextInt(16);
-            int blockY = world.getTopSolidBlockY(blockX, blockZ) - 1;
 
-            placeHouse(blockX, blockY, blockZ);
+            attemptToPlaceHouse(blockX, blockZ);
         }
     }
 
     @Unique
-    private void placeHouse(int blockX, int blockY, int blockZ) {
+    private void attemptToPlaceHouse(int blockX, int blockZ) {
+
+        int blockY = world.getTopSolidBlockY(blockX, blockZ) - 1;
+
+        if (world.getBlockId(blockX, blockY, blockZ) != Block.GRASS_BLOCK.id &&
+            world.getBlockId(blockX, blockY, blockZ) != Block.SAND.id)
+            return;
+
+        int floorId, wallId, columnId, roofMeta;
+
+        switch (this.world.method_1781().getBiome(blockX, blockZ).name) {
+
+            case "Desert":
+                floorId = Block.SANDSTONE.id;
+                wallId = Block.PLANKS.id;
+                columnId = Block.SANDSTONE.id;
+                roofMeta = 1;
+                break;
+
+            default:
+                floorId = Block.COBBLESTONE.id;
+                wallId = Block.PLANKS.id;
+                columnId = Block.LOG.id;
+                roofMeta = 3;
+                break;
+        }
 
         // floor and ceiling
         for (int xo = 0; xo < 5; xo++) {
             for (int zo = 0; zo < 5; zo++) {
 
-                world.setBlock(blockX + xo, blockY, blockZ + zo, Block.COBBLESTONE.id);
-                world.setBlock(blockX + xo, blockY + 4, blockZ + zo, Block.PLANKS.id);
+                world.setBlock(blockX + xo, blockY, blockZ + zo, floorId);
+                world.setBlock(blockX + xo, blockY + 4, blockZ + zo, wallId);
 
                 world.setBlock(blockX + xo, blockY + 5, blockZ + zo, Block.SLAB.id);
-                world.setBlockMeta(blockX + xo, blockY + 5, blockZ + zo, 3);
+                world.setBlockMeta(blockX + xo, blockY + 5, blockZ + zo, roofMeta);
+
+                // walls
+                if (xo == 0 || zo == 0 || xo == 4 || zo == 4)
+                    for (int yo = 1; yo < 4; yo++)
+                        world.setBlock(blockX + xo, blockY + yo, blockZ + zo, wallId);
+                else
+                    for (int yo = 1; yo < 4; yo++)
+                        world.setBlock(blockX + xo, blockY + yo, blockZ + zo, 0);
             }
         }
 
-        // walls
-        for (int yo = 1; yo < 4; yo++) {
-
-            world.setBlock(blockX, blockY + yo, blockZ + 1, Block.PLANKS.id);
-            world.setBlock(blockX, blockY + yo, blockZ + 2, Block.PLANKS.id);
-            world.setBlock(blockX, blockY + yo, blockZ + 3, Block.PLANKS.id);
-
-            world.setBlock(blockX + 4, blockY + yo, blockZ + 1, Block.PLANKS.id);
-            world.setBlock(blockX + 4, blockY + yo, blockZ + 2, Block.PLANKS.id);
-            world.setBlock(blockX + 4, blockY + yo, blockZ + 3, Block.PLANKS.id);
-
-            world.setBlock(blockX + 1, blockY + yo, blockZ, Block.PLANKS.id);
-            world.setBlock(blockX + 2, blockY + yo, blockZ, Block.PLANKS.id);
-            world.setBlock(blockX + 3, blockY + yo, blockZ, Block.PLANKS.id);
-
-            world.setBlock(blockX + 1, blockY + yo, blockZ + 4, Block.PLANKS.id);
-            world.setBlock(blockX + 2, blockY + yo, blockZ + 4, Block.PLANKS.id);
-            world.setBlock(blockX + 3, blockY + yo, blockZ + 4, Block.PLANKS.id);
-        }
-
+        // door/windows
         world.setBlock(blockX + 2, blockY + 2, blockZ + 4, 0);
         world.setBlock(blockX + 4, blockY + 2, blockZ + 2, 0);
         world.setBlock(blockX, blockY + 2, blockZ + 2, 0);
         world.setBlock(blockX + 2, blockY + 2, blockZ, 0);
+
         world.setBlock(blockX + 2, blockY + 1, blockZ, 0);
 
+        // torch
         world.setBlock(blockX + 2, blockY + 3, blockZ + 3, Block.TORCH.id);
 
         // log supports
         for (int yo = 0; yo < 5; yo++) {
 
-            world.setBlock(blockX + 4, blockY + yo, blockZ, Block.LOG.id);
-            world.setBlock(blockX + 4, blockY + yo, blockZ + 4, Block.LOG.id);
-            world.setBlock(blockX, blockY + yo, blockZ, Block.LOG.id);
-            world.setBlock(blockX, blockY + yo, blockZ + 4, Block.LOG.id);
+            world.setBlock(blockX + 4, blockY + yo, blockZ, columnId);
+            world.setBlock(blockX + 4, blockY + yo, blockZ + 4, columnId);
+            world.setBlock(blockX, blockY + yo, blockZ, columnId);
+            world.setBlock(blockX, blockY + yo, blockZ + 4, columnId);
         }
 
         WomanEntity resident = new WomanEntity(world);
