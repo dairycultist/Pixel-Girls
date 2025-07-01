14c25228e780c6ed448efcf80f162ed7a44da278
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-01 17:42:23 -0400

    Shortstack Miku

--- src/main/java/net/dairycultist/pixelgirls/model/ShortstackEntityModel.java
+++ src/main/java/net/dairycultist/pixelgirls/model/ShortstackEntityModel.java
@@ -14,24 +14,31 @@ public class ShortstackEntityModel extends BipedEntityModel {
         // pivot position  = global space
         // positive y = down
 
+        this.body = new ModelPart(0, 18);
         this.body.addCuboid(-4f, 0f, -2f, 8, 10, 4);
-        this.body.pivotY += 2f;
+        this.body.setPivot(0f, 2f, 0f);
 
+        this.leftArm = new ModelPart(24, 18);
+        this.leftArm.mirror = true;
         this.leftArm.addCuboid(-1f, -2f, -2f, 4, 10, 4);
-        this.leftArm.pivotY += 2f;
+        this.leftArm.setPivot(5f, 4f, 0f);
 
+        this.rightArm = new ModelPart(24, 18);
         this.rightArm.addCuboid(-3f, -2f, -2f, 4, 10, 4);
-        this.rightArm.pivotY += 2f;
+        this.rightArm.setPivot(-5f, 4f, 0f);
 
+        this.leftLeg = new ModelPart(44, 15);
+        this.leftLeg.mirror = true;
         this.leftLeg.addCuboid(-2.5f, 0f, -2.5f, 5, 12, 5);
-        this.leftLeg.pivotX += 1f;
+        this.leftLeg.setPivot(3f, 12f, 0f);
         this.leftLeg.roll = (float) (Math.PI / 16);
 
+        this.rightLeg = new ModelPart(44, 15);
         this.rightLeg.addCuboid(-2.5f, 0f, -2.5f, 5, 12, 5);
-        this.rightLeg.pivotX -= 1f;
+        this.rightLeg.setPivot(-3f, 12f, 0f);
         this.rightLeg.roll = (float) (-Math.PI / 16);
 
-        this.breasts = new ModelPart(40, 0);
+        this.breasts = new ModelPart(34, 0);
         this.breasts.addCuboid(0f, 0f, -5f, 10, 5, 5);
         this.breasts.setPivot(-5f, 3f, -2f);
         this.breasts.pitch = (float) (Math.PI / 8);
