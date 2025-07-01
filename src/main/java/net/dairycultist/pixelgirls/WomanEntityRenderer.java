589d9fcba48bee90c8cedff73df6cb9d3a9449e9
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-01 17:08:11 -0400

    Update variant system 2.3

--- src/main/java/net/dairycultist/pixelgirls/WomanEntityRenderer.java
+++ src/main/java/net/dairycultist/pixelgirls/WomanEntityRenderer.java
@@ -1,15 +1,10 @@
 package net.dairycultist.pixelgirls;
 
-import net.dairycultist.pixelgirls.model.ShortstackEntityModel;
 import net.dairycultist.pixelgirls.model.WomanEntityModel;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.entity.LivingEntity;
 
 public class WomanEntityRenderer extends LivingEntityRenderer {
-
-    private static final EntityModel MODEL_GENERIC = new WomanEntityModel();
-    private static final EntityModel MODEL_SHORTSTACK = new ShortstackEntityModel();
 
     public WomanEntityRenderer() {
         super(new WomanEntityModel(), 0.5f);
@@ -17,11 +12,7 @@ public class WomanEntityRenderer extends LivingEntityRenderer {
 
     public void render(LivingEntity livingEntity, double d, double e, double f, float g, float h) {
 
-        if (((WomanEntity) livingEntity).getVariant() == WomanVariants.MIKU) {
-            model = MODEL_SHORTSTACK;
-        } else {
-            model = MODEL_GENERIC;
-        }
+        model = ((WomanEntity) livingEntity).getVariant().model;
 
         super.render(livingEntity, d, e, f, g, h);
     }
--- src/main/java/net/dairycultist/pixelgirls/WomanVariants.java
+++ src/main/java/net/dairycultist/pixelgirls/WomanVariants.java
@@ -1,20 +1,26 @@
 package net.dairycultist.pixelgirls;
 
+import net.dairycultist.pixelgirls.model.ShortstackEntityModel;
+import net.dairycultist.pixelgirls.model.WomanEntityModel;
+import net.minecraft.client.render.entity.model.EntityModel;
+
 public enum WomanVariants {
 
-    MIKU("Miku"),
-    KOISHI("Koishi"),
-    SAKURA_MIKU("Sakura Miku"),
-    SENKO("Senko");
+    MIKU("Miku", new ShortstackEntityModel()),
+    KOISHI("Koishi", new WomanEntityModel()),
+    SAKURA_MIKU("Sakura Miku", new WomanEntityModel()),
+    SENKO("Senko", new WomanEntityModel());
 
     public static final WomanVariants[] OVERWORLD_POOL = { MIKU, KOISHI, SAKURA_MIKU };
     public static final WomanVariants[] NETHER_POOL = { SENKO };
 
     public final String name;
     public final int id;
+    public final EntityModel model;
 
-    WomanVariants(String name) {
+    WomanVariants(String name, EntityModel model) {
         this.name = name;
         this.id = this.ordinal();
+        this.model = model;
     }
 }
