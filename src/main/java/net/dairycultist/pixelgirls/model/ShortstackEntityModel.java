4958bc0d3f17c981773f5deab0ad79328b3e481c
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-01 17:49:46 -0400

    Make shortstacks very adorable

--- src/main/java/net/dairycultist/pixelgirls/model/ShortstackEntityModel.java
+++ src/main/java/net/dairycultist/pixelgirls/model/ShortstackEntityModel.java
@@ -21,22 +21,22 @@ public class ShortstackEntityModel extends BipedEntityModel {
         this.leftArm = new ModelPart(24, 18);
         this.leftArm.mirror = true;
         this.leftArm.addCuboid(-1f, -2f, -2f, 4, 10, 4);
-        this.leftArm.setPivot(5f, 4f, 0f);
+        this.leftArm.setPivot(4f, 4.5f, 0f);
 
         this.rightArm = new ModelPart(24, 18);
         this.rightArm.addCuboid(-3f, -2f, -2f, 4, 10, 4);
-        this.rightArm.setPivot(-5f, 4f, 0f);
+        this.rightArm.setPivot(-4f, 4.5f, 0f);
 
         this.leftLeg = new ModelPart(44, 15);
         this.leftLeg.mirror = true;
         this.leftLeg.addCuboid(-2.5f, 0f, -2.5f, 5, 12, 5);
         this.leftLeg.setPivot(3f, 12f, 0f);
-        this.leftLeg.roll = (float) (Math.PI / 16);
+        this.leftLeg.roll = (float) (Math.PI / 32);
 
         this.rightLeg = new ModelPart(44, 15);
         this.rightLeg.addCuboid(-2.5f, 0f, -2.5f, 5, 12, 5);
         this.rightLeg.setPivot(-3f, 12f, 0f);
-        this.rightLeg.roll = (float) (-Math.PI / 16);
+        this.rightLeg.roll = (float) (-Math.PI / 32);
 
         this.breasts = new ModelPart(34, 0);
         this.breasts.addCuboid(0f, 0f, -5f, 10, 5, 5);
@@ -47,6 +47,12 @@ public class ShortstackEntityModel extends BipedEntityModel {
     public void render(float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch, float scale) {
 
         this.setAngles(limbAngle, limbDistance, animationProgress, headYaw, headPitch, scale);
+
+        this.leftArm.roll += (float) (-Math.PI / 16);
+        this.rightArm.roll += (float) (Math.PI / 16);
+
+        this.leftArm.pivotX = 4f;
+        this.rightArm.pivotX = -4f;
 
         if (this.sneaking) {
             this.head.pivotY = 3.0F;
