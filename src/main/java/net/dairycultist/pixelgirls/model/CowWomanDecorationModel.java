b5a7c4c16d479fdd894697ca2149f4d57ddb80ba
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-03 20:21:50 -0400

    Add cow ears

--- src/main/java/net/dairycultist/pixelgirls/model/CowWomanDecorationModel.java
+++ src/main/java/net/dairycultist/pixelgirls/model/CowWomanDecorationModel.java
@@ -5,12 +5,25 @@ import net.minecraft.client.render.entity.model.EntityModel;
 
 public class CowWomanDecorationModel extends EntityModel {
 
+    public ModelPart leftEar; // +x
+    public ModelPart rightEar; // -x
     public ModelPart bell;
 
     public CowWomanDecorationModel(float headPivotY) {
         super();
 
-        // TODO add cow ears and cow horns
+        // TODO add cow horns
+
+        this.leftEar = new ModelPart(42, 4);
+        this.leftEar.mirror = true;
+        this.leftEar.addCuboid(3f, -4f, -0.5f, 4, 3, 1);
+        this.leftEar.setPivot(0f, headPivotY, 0f);
+        this.leftEar.roll = (float) (Math.PI / 16);
+
+        this.rightEar = new ModelPart(42, 4);
+        this.rightEar.addCuboid(-7f, -4f, -0.5f, 4, 3, 1);
+        this.rightEar.setPivot(0f, headPivotY, 0f);
+        this.rightEar.roll = (float) (-Math.PI / 16);
 
         this.bell = new ModelPart(0, 0);
         this.bell.addCuboid(-2.5f, 0f, 0f, 5, 2, 5);
@@ -19,6 +32,14 @@ public class CowWomanDecorationModel extends EntityModel {
 
     public void render(float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch, float scale) {
 
+        this.leftEar.yaw = headYaw / 57.295776f;
+        this.leftEar.pitch = headPitch / 57.295776f;
+
+        this.rightEar.yaw = headYaw / 57.295776f;
+        this.rightEar.pitch = headPitch / 57.295776f;
+
+        this.leftEar.render(scale);
+        this.rightEar.render(scale);
         this.bell.render(scale);
     }
 }
